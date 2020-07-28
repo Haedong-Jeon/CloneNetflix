23ca84632af4fb4a6ab24d48ab1630d1943caabb
Haedong-Jeon <haedongjeon@icloud.com>
2020-07-29 08:53:45 +0900

    Initial Commit

---
+++ CloneNetflix/CloneNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneNetflixApp.swift
+//  CloneNetflix
+//
+//  Created by 전해동 on 2020/07/29.
+//
+
+import SwiftUI
+
+@main
+struct CloneNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CloneNetflix/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CloneNetflix
+//
+//  Created by 전해동 on 2020/07/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
